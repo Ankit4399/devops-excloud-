620ddabb8eb8c715a2cacd12e84337d61ff47c97
Ankit4399 <ankitkumar380780@gmail.com>
2026-06-07 14:49:11 +0530

    basic express code tolearn deployment

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('server is healthy and running');
+}); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
